7929aaabea0bdbe8cebf7d0672f6d61997530eaf
nano713 <50872819+nano713@users.noreply.github.com>
2024-11-06 18:56:11 +0900

    Add suggetion

--- src/pymodaq_plugins_hinds/daq_move_plugins/daq_move_PEM200.py
+++ src/pymodaq_plugins_hinds/daq_move_plugins/daq_move_PEM200.py
@@ -4,11 +4,7 @@ from pymodaq.control_modules.move_utility_classes import DAQ_Move_base, comon_pa
     DataActuator  # common set of parameters for all actuators
 from pymodaq.utils.daq_utils import ThreadCommand # object used to send info back to the main thread
 from pymodaq.utils.parameter import Parameter
-from pymodaq_plugins_hindsinstruments.hardware.pem200_driver import PEM200Driver
-
-class PythonWrapperOfYourInstrument:
-    #  TODO Replace this fake class with the import of the real python wrapper of your instrument
-    pass
+from pymodaq_plugins_hinds.hardware.pem200_driver import PEM200Driver
 
 # TODO:
 # (1) change the name of the following class to DAQ_Move_TheNameOfYourChoice
@@ -40,14 +36,18 @@ class DAQ_Move_PEM200(DAQ_Move_base):
     """
     is_multiaxes = False  # TODO for your plugin set to True if this plugin is controlled for a multiaxis controller
     _axis_names: Union[List[str], Dict[str, int]] = ['Axis1', 'Axis2']  # TODO for your plugin: complete the list
-    _controller_units: Union[str, List[str]] = 'mm'  # TODO for your plugin: put the correct unit here, it could be
+    _controller_units: Union[str, List[str]] = 'nm'  # TODO for your plugin: put the correct unit here, it could be
     # TODO  a single str (the same one is applied to all axes) or a list of str (as much as the number of axes)
     _epsilon: Union[float, List[float]] = 0.1  # TODO replace this by a value that is correct depending on your controller
     # TODO it could be a single float of a list of float (as much as the number of axes)
     data_actuator_type = DataActuatorType.DataActuator  # wether you use the new data style for actuator otherwise set this
     # as  DataActuatorType.float  (or entirely remove the line)
 
-    params = [   # TODO for your custom plugin: elements to be added here as dicts in order to control your custom stage
+# DK - you need retardation, drive_value, state
+    params = [
+                 {'title': 'Resource Name:', 'name': 'resource_name', 'type': 'str', 'value': "ASRL6::INSTR"},
+                 {'title': 'Retardation:', 'name': 'retardation', 'type': 'float', 'value': 0.5},
+                 # TODO for your custom plugin: elements to be added here as dicts in order to control your custom stage
                 ] + comon_parameters_fun(is_multiaxes, axis_names=_axis_names, epsilon=_epsilon)
     # _epsilon is the initial default value for the epsilon parameter allowing pymodaq to know if the controller reached
     # the target value. It is the developer responsibility to put here a meaningful value
@@ -55,10 +55,10 @@ class DAQ_Move_PEM200(DAQ_Move_base):
     def ini_attributes(self):
         #  TODO declare the type of the wrapper (and assign it to self.controller) you're going to use for easy
         #  autocompletion
-        self.controller: PythonWrapperOfYourInstrument = None
+        self.controller: PEM200Driver = None
 
         #TODO declare here attributes you want/need to init with a default value
-        pass
+        # pass
 
     def get_actuator_value(self):
         """Get the current value from the hardware with scaling conversion.
@@ -68,25 +68,26 @@ class DAQ_Move_PEM200(DAQ_Move_base):
         float: The position obtained after scaling conversion.
         """
         ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        pos = DataActuator(data=self.controller.your_method_to_get_the_actuator_value())  # when writing your own plugin replace this line
+        # raise NotImplemented  # when writing your own plugin remove this line
+        pos = DataActuator(data=self.controller.get_modulation_amplitude())  # when writing your own plugin replace this line
         pos = self.get_position_with_scaling(pos)
         return pos
 
-    def user_condition_to_reach_target(self) -> bool:
-        """ Implement a condition for exiting the polling mechanism and specifying that the
-        target value has been reached
+    # def user_condition_to_reach_target(self) -> bool:
+    #     """ Implement a condition for exiting the polling mechanism and specifying that the
+    #     target value has been reached
+    #
+    #    Returns
+    #     -------
+    #     bool: if True, PyMoDAQ considers the target value has been reached
+    #     """
+    #     # TODO either delete this method if the usual polling is fine with you, but if need you can
+    #     #  add here some other condition to be fullfilled either a completely new one or
+    #     #  using or/and operations between the epsilon_bool and some other custom booleans
+    #     #  for a usage example see DAQ_Move_brushlessMotor from the Thorlabs plugin
+    #     return True
 
-       Returns
-        -------
-        bool: if True, PyMoDAQ considers the target value has been reached
-        """
-        # TODO either delete this method if the usual polling is fine with you, but if need you can
-        #  add here some other condition to be fullfilled either a completely new one or
-        #  using or/and operations between the epsilon_bool and some other custom booleans
-        #  for a usage example see DAQ_Move_brushlessMotor from the Thorlabs plugin
-        return True
-
+    # DK - call close method
     def close(self):
         """Terminate the communication protocol"""
         ## TODO for your custom plugin
@@ -102,14 +103,23 @@ class DAQ_Move_PEM200(DAQ_Move_base):
             A given parameter (within detector_settings) whose value has been changed by the user
         """
         ## TODO for your custom plugin
-        if param.name() == 'axis':
-            self.axis_unit = self.controller.your_method_to_get_correct_axis_unit()
+        if param.name() == 'state':
+            # self.controller... # DK - edit this line
+
             # do this only if you can and if the units are not known beforehand, for instance
             # if the motors connected to the controller are of different type (mm, µm, nm, , etc...)
             # see BrushlessDCMotor from the thorlabs plugin for an exemple
 
+            pass # DK delete pass and write some method
+
+        # DK - add elif ... "retardation"
+
+
+        # DK - add elif ... "state"
         elif param.name() == "a_parameter_you've_added_in_self.params":
            self.controller.your_method_to_apply_this_param_change()
+
+        # DK - add elif ... "drive_value"
         else:
             pass
 
@@ -127,18 +137,22 @@ class DAQ_Move_PEM200(DAQ_Move_base):
         initialized: bool
             False if initialization failed otherwise True
         """
-        raise NotImplemented  # TODO when writing your own plugin remove this line and modify the ones below
+        # raise NotImplemented  # TODO when writing your own plugin remove this line and modify the ones below
         self.ini_stage_init(slave_controller=controller)  # will be useful when controller is slave
 
         if self.is_master:  # is needed when controller is master
-            self.controller = PythonWrapperOfYourInstrument(arg1, arg2, ...) #  arguments for instantiation!)
+            self.controller = PEM200Driver(self.settings["resource_name"]) #  arguments for instantiation!)
+            self.controller.connect()
+
             # todo: enter here whatever is needed for your controller initialization and eventual
             #  opening of the communication channel
 
-        info = "Whatever info you want to log"
-        initialized = self.controller.a_method_or_atttribute_to_check_if_init()  # todo
+        info = "Whatever info you want to log" # DK - edit this line
+
+        initialized = True
         return info, initialized
 
+    # DK - Use set_modulation_amplitude
     def move_abs(self, value: DataActuator):
         """ Move the actuator to the absolute target defined by value
 
@@ -164,21 +178,23 @@ class DAQ_Move_PEM200(DAQ_Move_base):
         """
         value = self.check_bound(self.current_position + value) - self.current_position
         self.target_value = value + self.current_position
-        value = self.set_position_relative_with_scaling(value)
+        value = self.set_position_with_scaling(self.target_value)
+        # value = self.set_position_relative_with_scaling(value)
 
         ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        self.controller.your_method_to_set_a_relative_value(value.value())  # when writing your own plugin replace this line
-        self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
+        # raise NotImplemented  # when writing your own plugin remove this line
+        self.controller.set_modulation_amplitude(value.value())  # when writing your own plugin replace this line
+        # self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
 
     def move_home(self):
         """Call the reference method of the controller"""
 
         ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        self.controller.your_method_to_get_to_a_known_reference()  # when writing your own plugin replace this line
-        self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
+        # raise NotImplemented  # when writing your own plugin remove this line
+        self.controller.set_modulation_amplitude(0)  # when writing your own plugin replace this line
+        # self.emit_status(ThreadCommand('Update_Status', ['Some info you want to log']))
 
+    # DK - turn off by set_pem_output
     def stop_motion(self):
       """Stop the actuator and emits move_done signal"""
 
